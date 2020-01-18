6d413197c2135b388b3a4f5738d758ace251cf51
pzflying357 <369284860@qq.com>
2020-01-18 12:48:24 +0800

    Create 20200118.py
    0115

---
+++ 20200118.py
@@ -0,0 +1,17 @@
+#ex1
+num=input('请猜一个数字：')
+print('你猜的数字是：%s'%(num))
+
+#ex2
+money = 20
+# 将 money 变量的值乘以 2 再加上 50
+# 赋值给 result
+result = money*2 + 50
+print(result)
+
+#ex3
+a = 'hello,world'  # str
+b = 1.414          #float
+c = (1 != 2)       #bool Ture
+d = (True and False) #bool  False
+e = (True or False)  #bool  Ture
