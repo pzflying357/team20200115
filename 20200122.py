dbb2e63a2a2a7d07d023633c3c4d75b241afcb1d
pzflying357 <369284860@qq.com>
2020-01-23 13:15:20 +0800

    Create 20200122.py
    0122

---
+++ 20200122.py
@@ -0,0 +1,27 @@
+#ex1
+name = 'Crossin'
+age = 18
+code = 'Python'
+
+result =('%s is %d , he writes %s' %(name,age,code))
+print(result)
+
+
+#ex2
+num1 = '3.3'
+num2 = 2.5
+num1 =float(num1)
+
+
+print(num1 + num2)
+
+#ex3
+bool(-123) #True
+bool(0)     #False
+bool('abc')  #True
+bool('False')  #True
+bool('')    #False
+bool([])    #False
+bool({})    #False
+bool([''])  #True
+bool(None)  #False
